67f7a02f2829b585eb39b52cc445716b14dc269f
Arthur <58260630+artoer@users.noreply.github.com>
2020-01-18 21:21:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,17 @@
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
 
+//de zoekknop class wordt toegevoegd aan de variabele "zoeken"
 var zoeken = document.querySelector('.zoekknop');
+//de zoekbalk class wordt toegevoegd aan de variable "searchbar"
 var searchbar = document.querySelector('.search-closed');
 
+//wanneer de eventlistener wordt getriggerd wordt de class van de zoekbalk getoggled
 function toggle() {
     searchbar.classList.toggle('search-open');
 }
 
+//er wordt een eventlistener toegevoegd aan de zoekknop
 zoeken.addEventListener("click", toggle);
 
 console.log(zoeken, searchbar, toggle);
